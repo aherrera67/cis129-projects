b5a8a874a84b6d5bf8d2f0160a36577e6953c588
aherrera67 <159605786+aherrera67@users.noreply.github.com>
2024-02-09 23:27:31 -0700

    Update cis129_lab02_shortStory.py
    made a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Ana Herrera
+# Date: 2/9/24
+# Description: A program using print to create a short story.
+
+print("Once upon a time lived a girl in her home.")
+print("She was lonely and sad.")
+print("Then one faithful day, she found a small puppy on the side of the road.")
+print("She brought her to her home and named her Nube.")
+print("The girl is no longer lonely and has a bestfriend!")
